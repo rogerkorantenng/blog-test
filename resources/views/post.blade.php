5521f202a6d8e5e299d8bf874393b768cefb0d00
rogerkorantenng <rogerkorantenng@gmail.com>
2023-11-06 15:24:54 +0000

    Initial Commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+    <!doctype html>
+        <title>My Blog</title>
+        <link rel="stylesheet" href="/app.css">
+          <body>
+               <article>
+                   <?= $post; ?>
+               </article>
+               <a href = "/">Go Back<a> 
+          </body>   
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+    <body>
+        <article>
+            <h1>
+                <a href="/post">My First Post</a>
+            </h1>
+                <p>
+                    Millions of customers, including thousands of the fastest-growing startups, use AWS to build fast, keep costs
+                    low, and prove what’s possible.
+                </p>
+        </article>
+
+
+        <article>
+            <h1>
+                <a href="/post">My Second Post</a>
+            </h1>
+                <p>
+                    Millions of customers, including thousands of the fastest-growing startups, use AWS to build fast, keep
+                    costs low, and prove what’s possible.
+                </p>
+        </article>
+
+        <article>
+            <h1>
+                <a href="/post">My Third Post</a>
+            </h1>
+                <p>
+                    Millions of customers, including thousands of the fastest-growing startups, use AWS to build fast, keep costs
+                    low, and prove what’s possible.
+                </p>
+        </article>
+    </body>
